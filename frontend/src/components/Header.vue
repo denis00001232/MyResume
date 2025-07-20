4d0d2d6a3a4ebd0ce741a62f6f81ed9115b44e8b
denis <denis-savchenko2003@mail.ru>
2025-07-20 20:36:11 +0300

    initial

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<template>
+<div class="background">
+  <div class="text-container">
+    <div class="text">Денис Савченко</div>
+  </div>
+  <div class="text-container">
+    <div class="text">Full Stack разработчик с двумя годами опыта</div>
+  </div>
+  <div></div>
+</div>
+</template>
+
+<style scoped>
+.background {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 5vh 5vw;
+  gap: 5vw;
+}
+.text {
+  font-size: 20px;
+  color: aliceblue;
+}
+.text-container {
+  padding: 10px 5px;
+  background: cadetblue;
+  border-radius: 10px;
+}
+</style>
